6830a5c89c7a86f6f30ead318dd10591559f955d
unknown <alfredo.goeten@outlook.com>
2021-11-20 15:31:15 -0300

    added sprites

--- src/main.js
+++ src/main.js
@@ -24,11 +24,37 @@ const config = {
 
 const game = new Phaser.Game(config)
 
+let ball
+let paddleLeft
+let paddleRight
+
 function preload() {
+
+    this.load.image('ball', require('../assets/ball.png'))
+    this.load.image('paddleLeft', require('../assets/leftcat.png'))
+    this.load.image('paddleRight', require('../assets/rightcat.png'))
 
 }
 
 function create() {
+
+    ball = this.physics.add.sprite(
+        this.physics.world.bounds.width / 2,
+        this.physics.world.bounds.height / 2,
+        'ball'
+    )
+
+    paddleLeft = this.physics.add.sprite(
+        ball.body.width / 2 + 40,
+        this.physics.world.bounds.height / 2,
+        'paddleLeft'
+    )
+
+    paddleRight = this.physics.add.sprite(
+        this.physics.world.bounds.width - (ball.body.width / 2 + 40),
+        this.physics.world.bounds.height / 2,
+        'paddleRight'
+    )
 
 }
 
